d594ef3eef81b06663d07c9458581f219a07762e
perezcruzf <33552032+perezcruzf@users.noreply.github.com>
2017-11-10 16:18:07 +0000

    Update sqsPost.py
    Removed AWS access, local secret retrieval

--- sqsPost.py
+++ sqsPost.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 
-import boto3
+
+# import boto3
 import os
 import json
 
-queue_url = "https://sqs.us-east-1.amazonaws.com/085857639847/JenkinsWorldDemo.fifo" 
+#queue_url = "https://sqs.eu-west-2.amazonaws.com/860708036433/JenkinsWorldDemo" 
 
 access_key = os.environ['AWS_ACCESS_KEY_ID']
 secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
-sqs_client = boto3.client('sqs', region_name='us-east-1')
+# sqs_client = boto3.client('sqs', region_name='eu-west-2')
 
-sqs_message = "{\n   AccessKeyID : " + access_key + ",\n   SecretAccessKey : " + secret_key + "\n}"
+sqs_message = "{\n The Access Key ID is: " + access_key + ",\n   The Secret Access Key is: " + secret_key + "\n}"
 print sqs_message
-sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
+# sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
